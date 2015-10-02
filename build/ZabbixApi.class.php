248223ded685a04b77564309ba73ee84f7c8e16b
Dominique Barton <dbarton@confirm.ch>
2015-10-02 19:39:27 +0200

    built libraries

---
+++ build/ZabbixApi.class.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @file    ZabbixApi.class.php
+ * @brief   Class file for the implementation of the class ZabbixApi.
+ *
+ * Implement your customizations in this file.
+ *
+ * This file is part of PhpZabbixApi.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @copyright   The MIT License (MIT)
+ * @author      confirm IT solutions GmbH, Rathausstrase 14, CH-6340 Baar
+ */
+
+namespace ZabbixApi;
+
+require 'ZabbixApiAbstract.class.php';
+
+/**
+ * @brief   Concrete class for the Zabbix API.
+ */
+
+class ZabbixApi extends ZabbixApiAbstract
+{
+
+}
+
+?>
